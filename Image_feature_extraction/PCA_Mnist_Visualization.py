015ce225bb83222c2be275186ef36b71255fb62f
Jeremy12106 <jeremy12106@gmail.com>
2024-09-10 13:30:51 +0800

    update image feature extraction

--- Image_feature_extraction/PCA_Mnist_Visualization.py
+++ Image_feature_extraction/PCA_Mnist_Visualization.py
@@ -2,20 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-fc1=np.load('ANN0.npy')
+fc1=np.load('assets/ANN0.npy')
 fc2=np.random.randn(128*10).reshape([128,10])
 
-label=np.load('mnistLabel.npy')
-input=np.load('mnist.npy')/255.0
+label=np.load('assets/mnistLabel.npy')
+input=np.load('assets/mnist.npy')/255.0
 input=np.reshape(input,[len(input),-1])
 
-# label=np.load('cifarLabel.npy')
-# input=np.load('cifar.npy')
-
-########################################Onehot Encode
+####################    Onehot Encode   #######################################
 D=np.identity(10)[label]
-########################################End code
+#-------------------    End code    -------------------------------------------
 
 input2=input[-10000:]
 D2=D[-10000:]
@@ -24,20 +20,14 @@ D=D[0:-10000]
 label2=label[-10000:]
 label=label[0:-10000]
 
-
-
 plt.figure(figsize=(15,10))
 plt.subplot(231)
-
-
 
 X=input.copy().astype(float)
 cov=np.cov(X.T)
 val,vec=np.linalg.eig(cov)
 principle=np.argsort(-val)[0:2]
 dim2=np.dot(X,vec[:,principle])
-
-
 
 Alpha=1
 size=1
@@ -46,8 +36,6 @@ for cl in range(10):
     plt.scatter(chos[:,0],chos[:,1],label=str(cl),alpha=Alpha,marker='x',s=size)
 plt.legend()
 plt.title('Input Component')
-
-
 
 input=255*(input-np.min(input,1).reshape([-1,1]))/(np.max(input,1).reshape([-1,1])-np.min(input,1).reshape([-1,1]))
 
@@ -111,13 +99,12 @@ d=d.real
 d=d.reshape([10,10,28,28]).transpose([0,2,1,3]).reshape([280,280]).astype(np.uint8)
 plt.imshow(d)
 
-'''
+
 rec=[]
 rec2=[]
-EP=200
+EP=100
 
 for ep in range(EP):
-    
     
     X1=np.dot(fc1.T,input2.T).T
 
@@ -128,22 +115,15 @@ for ep in range(EP):
     A2=1/(1+np.exp(-X2))    
     choose2=np.argmax(A2,1)
     
-    
-    
-    
 ########################################Forward Pass X1=???
     X1=np.dot(fc1.T,input.T).T
 ########################################End code
     
-    
-    
-    
+
 ########################################Forward Pass A1=???
     A1=X1.copy()
     A1[A1<0]=0
 ########################################End code
-    
-    
     
     
 ########################################Forward Pass X2=???
@@ -151,12 +131,9 @@ for ep in range(EP):
 ########################################End code
     
     
-    
-    
 ########################################Forward Pass A2=???
     A2=1/(1+np.exp(-X2))
 ########################################End code
-    
     
     
 ########################################Backward delta=???
@@ -164,41 +141,30 @@ for ep in range(EP):
 ########################################End code
     
     
-    
 ########################################Backward grad=???
     grad=-2*np.dot(A1.T,delta)+0.001*fc2
 ########################################End code
     
-    
-    
+
     fc2=fc2-0.05*grad
     
     
     choose=np.argmax(A2,1)
     print(str(ep)+" TraningACC: "+str(np.sum(choose==label)/len(label))+' TestingACC:'+str(np.sum(choose2==label2)/len(label2)))
     rec.append(np.sum(choose==label)/len(label))
-#    
-#
-#
 
-#    print(str(ep)+": "+str(np.sum(choose2==label2)/len(label2)))
+    # print(str(ep)+": "+str(np.sum(choose2==label2)/len(label2)))
     rec2.append(np.sum(choose2==label2)/len(label2))
-    
-    
-    
-    
+     
 plt.plot(np.arange(EP),rec,label='TrainingACC')
 plt.plot(np.arange(EP),rec2,label='TestingACC')
 plt.legend()
 plt.xlabel('Epoch')
 plt.ylabel('ACC')
 
-
-
-
 plt.figure(figsize=(15,10))
 plt.subplot(231)
 
-'''
+
 
 
